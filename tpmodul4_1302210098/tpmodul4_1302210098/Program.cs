b695a4b843245723e16c4f44eadfadb1c94b2c65
alifrasyid <alifrasyid.271003@gmail.com>
2023-03-12 23:47:06 +0700

    Memperbaiki pada code table-driven

--- tpmodul4_1302210098/tpmodul4_1302210098/Program.cs
+++ tpmodul4_1302210098/tpmodul4_1302210098/Program.cs
@@ -4,53 +4,13 @@ public class KodePos
 {
     public enum Kelurahan{Batununggal,Kujangsari,Mengger,Wates,Cijaura,Jatisari,Margasari,Sekejati,Kebonwaru,Maleer,Samoja}
 
-    public String getKodePos(Kelurahan kelurahan)
+    public static int getKodePos(Kelurahan KELURAHAN)
     {
-        string kodepos = "";
-
-        switch (kelurahan)
-        {
-            case Kelurahan.Batununggal:
-                kodepos = "40266";
-                break;
-            case Kelurahan.Kujangsari:
-                kodepos = "40287";
-                break;
-            case Kelurahan.Mengger:
-                kodepos = "40267";
-                break;
-            case Kelurahan.Wates:
-                kodepos = "40256";
-                break;
-            case Kelurahan.Cijaura:
-                kodepos = "40287";
-                break;
-            case Kelurahan.Jatisari:
-                kodepos = "40286";
-                break;
-            case Kelurahan.Margasari:
-                kodepos = "40286";
-                break;
-            case Kelurahan.Sekejati:
-                kodepos = "40286";
-                break;
-            case Kelurahan.Kebonwaru:
-                kodepos = "40272";
-                break;
-            case Kelurahan.Maleer:
-                kodepos = "40274";
-                break;
-            case Kelurahan.Samoja:
-                kodepos = "40273";
-                break;
-            default:
-                kodepos = "Kode pos untuk kelurahan tersebut tidak ditemukan.";
-                break;
-        }
-
-        return kodepos;
+        int[] kodepos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+        return kodepos[(int)KELURAHAN];
     }
 }
+
 public class DoorMachine
 {
     enum DoorState {Terkunci,Terbuka}
@@ -84,7 +44,7 @@ class MainClass
     {
         KodePos kodePosK = new KodePos();
         KodePos.Kelurahan kelurahan = KodePos.Kelurahan.Kujangsari;
-        string kodepos = kodePosK.getKodePos(kelurahan);
+        int kodepos = KodePos.getKodePos(KodePos.Kelurahan.Kujangsari);
         Console.WriteLine("Kode pos untuk kelurahan "+kelurahan+" adalah "+ kodepos);
 
         DoorMachine dm = new DoorMachine();
